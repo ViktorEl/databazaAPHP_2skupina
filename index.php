25a2aa0c590f80c0ffbe0fe78e5284dbe07c8a76
Your Name <you@example.com>
2025-02-07 08:24:20 +0100

    vytvorenie index.php a vytvorenie formulara

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pripajanie do DB</h1>
+
+    <form action="index.php" method="POST">
+        <input type="text" name="meno" placeholder="zadajte meno" required> <br>
+        <input type="password" name="heslo" placeholder="zadajte heslo" required> <br>
+        <input type="submit" name="tlacidlo" value="Odoslat">
+    </form>
+
+
+</body>
+</html>
