46d018764b819a51ebd6c765390c71fd324e088e
Your Name <you@example.com>
2025-03-21 09:16:44 +0100

    dokoncenie skriptu na vypisovanie udajov z databazy + vytvorenie tabulky

--- vypis.php
+++ vypis.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <form action="vypis.php" method="GET">
         <input type="submit" name="tlacidlo" value="Vypíš data"> <br>
-    </form>
+    </form> 
 
 
     <?php
@@ -19,16 +20,31 @@
             $db_nazov = "tesco";  
             
             $pripojenie = mysqli_connect($db_server, $db_meno, $db_heslo, $db_nazov);
+            mysqli_set_charset($pripojenie, "utf8");
 
             $dotaz = "SELECT * FROM tovar";
 
             $prijem = mysqli_query($pripojenie, $dotaz);
 
-            while($riadok = mysqli_fetch_assoc($prijem)) {
-                echo $riadok["nazov"]." ".$riadok["cena"]." ".$riadok["mnozstvo"]." ".$riadok["spotreba"];
-                echo "<br>";
-            }
+            
+            echo "<table>";
+                echo "<caption>Tovar v Tescu </caption>";
+                echo "<tr>
+                        <th>tovar</th>
+                        <th>cena</th>
+                        <th>mnozstvo</th>
+                        <th>spotreba</th>
+                </tr>";
 
+            while($riadok = mysqli_fetch_assoc($prijem)) {
+                echo "<tr>
+                        <td>".$riadok["nazov"]."</td>
+                        <td>".$riadok["cena"]."</td>
+                        <td>".$riadok["mnozstvo"]."</td>
+                        <td>".$riadok["spotreba"]."</td>
+                </tr>";
+            }
+            echo "</table>";
         }
 
     ?>
