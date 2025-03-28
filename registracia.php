d78d9680e945a2d042573f2f854e9e806e82a35f
Your Name <you@example.com>
2025-03-28 08:35:03 +0100

    Odoslanie do db

--- registracia.php
+++ registracia.php
@@ -14,6 +14,38 @@
         <input type="submit" name="button" value="Registruj">
     </form>
 
+    <?php
+        if(isset($_POST["button"])) {
+            if(isset($_POST["login"]) && isset($_POST["pass"])) {
+                $db_server = "localhost";
+                $db_login = "root";
+                $db_password = "vertrigo";
+                $db_name = "registracia";
+
+                $connection = mysqli_connect($db_server, $db_login, $db_password, $db_name);
+                mysqli_set_charset($connection, "utf8");
+                $login = $_POST["login"];
+                $login = mysqli_real_escape_string($connection, $login); // ochrana proti SQL injection
+                $password = $_POST["pass"];
+                $password = mysqli_real_escape_string($connection, $password);
+
+                $query = "INSERT INTO persons (userName, passwd) VALUES ('$login', '$password')";
+                $result = mysqli_query($connection, $query);
+                if(!$result) {
+                    die("chyba registracie");
+                }
+                else {
+                    echo "Registracia prebehla úspešne";
+                }
+
+            }
+        }
+
+
+
+
+
+    ?>
 
 
 
