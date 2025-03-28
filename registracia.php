b22d35c2d86305d9e7c7e17b7f10ba9420118ea3
Your Name <you@example.com>
2025-03-28 08:15:21 +0100

    registracia.php uloha na ochranu proti sql injection

---
+++ registracia.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Registrácia</h2>
+
+    <form action="registracia.php" method="post">
+        <input type="text" name="login" placeholder="Pouzivatelske meno" required> <br>
+        <input type="password" name="pass" placeholder="Pouzivatelske heslo" required> <br>
+        <input type="submit" name="button" value="Registruj">
+    </form>
+
+
+
+
+
+    
+</body>
+</html>
