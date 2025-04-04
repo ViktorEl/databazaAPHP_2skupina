b2f35effb8be1c955db23f85f6744110d71c20e1
Your Name <you@example.com>
2025-04-04 08:52:12 +0200

    prihlasenie.php - formular

---
+++ prihlasenie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Prihlaste sa</h1>
+
+    <form action="prihlasenie.php" method="">
+        <label>Prihlasovacie meno</label> <input type="text" name="meno" placeholder="Zadajte meno" required><br>
+        <label>Prihlasovacie heslo</label> <input type="password" name="heslo" placeholder="Zadajte heslo" required> <br>
+        <input type="submit" name="tlacidlo" value="Prihlásiť">
+    </form>
+
+    
+</body>
+</html>
